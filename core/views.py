848e5a9fb81ed23c7277550f56ecaa7e699839c0
ben <bjblue@quinnipiac.edu>
2015-11-13 04:54:33 +0000

    Add movie create view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
+
+class MovieCreateView(CreateView):
+    model = Movie
+    template_name = "movie/movie_form.html"
+    fields = ['title', 'synopsis']
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(MovieCreateView, self).form_valid(form)
