1daafa4ce44918672970518bb8711f5ae1db11e9
ben <bjblue@quinnipiac.edu>
2015-11-15 21:05:19 +0000

    Add movie update view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
 
@@ -24,3 +24,8 @@ class MovieListView(ListView):
 class MovieDetailView(DetailView):
     model = Movie
     template_name = "movie/movie_detail.html"
+    
+class MovieUpdateView(UpdateView):
+    model = Movie
+    template_name = 'movie/movie_form.html'
+    fields = ['title', 'synopsis']
