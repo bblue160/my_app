5d49db0e92b22fc73afa0febbfa16cb9eb4e5eed
ben <bjblue@quinnipiac.edu>
2015-11-16 22:06:23 +0000

    Prevent users from submitting multiple reviews

--- core/views.py
+++ core/views.py
@@ -31,24 +31,26 @@ class MovieDetailView(DetailView):
         movie = Movie.objects.get(id=self.kwargs['pk'])
         reviews = Review.objects.filter(movie=movie)
         context['reviews'] = reviews
+        user_reviews = Review.objects.filter(movie=movie, user=self.request.user)
+        context['user_reviews'] = user_reviews
         return context
 
 class MovieUpdateView(UpdateView):
     model = Movie
     template_name = 'movie/movie_form.html'
     fields = ['title', 'synopsis']
-    
+
     def get_object(self, *args, **kwargs):
         object = super(MovieUpdateView, self).get_object(*args, **kwargs)
         if object.user != self.request.user:
             raise PermissionDenied()
-        return object   
+        return object
 
 class MovieDeleteView(DeleteView):
     model = Movie
     template_name = 'movie/movie_confirm_delete.html'
     success_url = reverse_lazy('movie_list')
-    
+
     def get_object(self, *args, **kwargs):
         object = super(MovieDeleteView, self).get_object(*args, **kwargs)
         if object.user != self.request.user:
@@ -64,6 +66,9 @@ class ReviewCreateView(CreateView):
         return self.object.movie.get_absolute_url()
 
     def form_valid(self, form):
+        movie = Movie.objects.get(id=self.kwargs['pk'])
+        if Review.objects.filter(movie=movie, user=self.request.user).exists():
+            raise PermissionDenied()
         form.instance.user = self.request.user
         form.instance.movie = Movie.objects.get(id=self.kwargs['pk'])
         return super(ReviewCreateView, self).form_valid(form)
@@ -76,21 +81,21 @@ class ReviewUpdateView(UpdateView):
 
     def get_success_url(self):
         return self.object.movie.get_absolute_url()
-      
+
     def get_object(self, *args, **kwargs):
         object = super(ReviewUpdateView, self).get_object(*args, **kwargs)
         if object.user != self.request.user:
             raise PermissionDenied()
-        return object 
-      
+        return object
+
 class ReviewDeleteView(DeleteView):
     model = Review
     pk_url_kwarg = 'review_pk'
     template_name = 'review/review_confirm_delete.html'
-    
+
     def get_success_url(self):
         return self.object.movie.get_absolute_url()
-      
+
     def get_object(self, *args, **kwargs):
         object = super(ReviewDeleteView, self).get_object(*args, **kwargs)
         if object.user != self.request.user:
