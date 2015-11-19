1ece835b66206fac1ac250daec7cd71651084c69
ben <bjblue@quinnipiac.edu>
2015-11-19 19:33:00 +0000

    Add user profiles

--- core/urls.py
+++ core/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
     url(r'^movie/(?P<pk>\d+)/review/create/$', login_required(ReviewCreateView.as_view()), name='review_create'),
     url(r'^movie/(?P<movie_pk>\d+)/review/update/(?P<review_pk>\d+)/$', login_required(ReviewUpdateView.as_view()), name='review_update'), 
     url(r'^movie/(?P<movie_pk>\d+)/review/delete/(?P<review_pk>\d+)/$', login_required(ReviewDeleteView.as_view()), name='review_delete'),
-    url(r'^vote/$', login_required(VoteFormView.as_view()), name='vote'),                   
+    url(r'^vote/$', login_required(VoteFormView.as_view()), name='vote'),
+    url(r'^user/(?P<slug>\w+)/$', login_required(UserDetailView.as_view()), name='user_detail'),                   
 )
